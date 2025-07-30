b146d6b887bda0b227ef74774981b188b9fd7fff
Ngene <hplxg@hotmail.com>
2025-07-30 11:39:58 +0100

    test: shapegen

--- minimal_demo.py
+++ minimal_demo.py
@@ -16,11 +16,11 @@ from PIL import Image
 
 from hy3dgen.rembg import BackgroundRemover
 from hy3dgen.shapegen import Hunyuan3DDiTFlowMatchingPipeline
-from hy3dgen.texgen import Hunyuan3DPaintPipeline
+# from hy3dgen.texgen import Hunyuan3DPaintPipeline
 
 model_path = 'tencent/Hunyuan3D-2'
 pipeline_shapegen = Hunyuan3DDiTFlowMatchingPipeline.from_pretrained(model_path)
-pipeline_texgen = Hunyuan3DPaintPipeline.from_pretrained(model_path)
+# pipeline_texgen = Hunyuan3DPaintPipeline.from_pretrained(model_path)
 
 image_path = 'assets/demo.png'
 image = Image.open(image_path).convert("RGBA")
@@ -29,5 +29,5 @@ if image.mode == 'RGB':
     image = rembg(image)
 
 mesh = pipeline_shapegen(image=image)[0]
-mesh = pipeline_texgen(mesh, image=image)
+# mesh = pipeline_texgen(mesh, image=image)
 mesh.export('demo.glb')

261aed2eb069d6d04e13b85bc394e531e04c5044
Ngene <hplxg@hotmail.com>
2025-08-13 16:53:35 +0100

    test: shapegen

--- hy3dgen/shapegen/models/autoencoders/attention_blocks.py
+++ hy3dgen/shapegen/models/autoencoders/attention_blocks.py
@@ -594,6 +594,12 @@ class PointCrossAttentionEncoder(nn.Module):
         random_query_ratio = num_random_query / input_random_pc_size
         idx_random_pc = torch.randperm(random_pc.shape[1], device=random_pc.device)[:input_random_pc_size]
         input_random_pc = random_pc[:, idx_random_pc, :]
+
+        print('>' * 20, f"input_random_pc.shape: {input_random_pc.shape}")
+        print('>' * 20, f"B: {B}")
+        print('>' * 20, f"input_random_pc_size: {input_random_pc_size}")
+        print('>' * 20, f"D: {D}")
+
         flatten_input_random_pc = input_random_pc.view(B * input_random_pc_size, D)
         N_down = int(flatten_input_random_pc.shape[0] / B)
         batch_down = torch.arange(B).to(pc.device)

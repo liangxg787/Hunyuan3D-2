e240a072a088111ac0419c398badc676a34cb6dd
Ngene <hplxg@hotmail.com>
2025-08-13 17:00:41 +0100

    test: shapegen

--- hy3dgen/shapegen/models/autoencoders/attention_blocks.py
+++ hy3dgen/shapegen/models/autoencoders/attention_blocks.py
@@ -590,6 +590,11 @@ class PointCrossAttentionEncoder(nn.Module):
                    1] <= self.pc_sharpedge_size, "Sharpedge surface points size must be less than or equal to pc_sharpedge_size"
 
         # Randomly select random surface points and random query points
+        print('>' * 20, f"self.pc_size: {self.pc_size}")
+        print('>' * 20, f"num_latents: {num_latents}")
+        print('>' * 20, f"self.pc_sharpedge_size: {self.pc_sharpedge_size}")
+        print('>' * 20, f"num_random_query.shape: {num_random_query}")
+        print('>' * 20, f"self.downsample_ratio: {self.downsample_ratio}")
         input_random_pc_size = int(num_random_query * self.downsample_ratio)
         random_query_ratio = num_random_query / input_random_pc_size
         idx_random_pc = torch.randperm(random_pc.shape[1], device=random_pc.device)[:input_random_pc_size]
